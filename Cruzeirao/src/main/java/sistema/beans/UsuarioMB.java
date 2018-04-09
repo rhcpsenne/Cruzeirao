173ca31e4f7c951767ca429caeb08646f48b3b97
Lucas <Lucas@DESKTOP-6VG8PQP>
2018-04-08 21:07:18 -0300

    Alguns ajustes

---
+++ Cruzeirao/src/main/java/sistema/beans/UsuarioMB.java
@@ -0,0 +1,53 @@
+package sistema.beans;
+
+import sistema.modelos.Usuario;
+import sistema.service.UsuarioService;
+
+import java.util.List;
+
+import org.primefaces.event.RowEditEvent;
+
+public class UsuarioMB {
+	private Usuario usuario = new Usuario();
+	private List<Usuario> usuarios;
+	private UsuarioService service = new UsuarioService();
+	
+	public void onRowEdit(RowEditEvent event) {
+
+		Usuario a = ((Usuario) event.getObject());
+		service.alterar(a);
+	}
+
+	public void salvar() {
+		usuario = service.salvar(usuario);
+
+		if (usuarios != null)
+			usuarios.add(usuario);
+
+		usuario = new Usuario();
+
+	}
+
+	public Usuario getAluno() {
+		return usuario;
+	}
+
+	public void setAluno(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	// Retorna a lista de alunos para a tabela
+	public List<Usuario> getUsuarios() {
+		if (usuarios == null)
+			usuarios = service.getUsuarios();
+
+		return usuarios;
+	}
+
+	public void remover(Usuario usuario) {
+		service.remover(usuario);
+		usuarios.remove(usuario);
+
+	}
+	
+}

25220d3fc08696704053f9043f70f8bfad2e52a9
VITOR GABRIEL DE OLIVEIRA PELEGRINETTI <160015@LABINFO.FACENS.BR>
2018-06-12 22:35:27 -0300

    Cadastros quase funcionando

--- Cruzeirao/src/main/java/sistema/converter/EquipeConverter.java
+++ Cruzeirao/src/main/java/sistema/converter/EquipeConverter.java
@@ -3,10 +3,12 @@ package sistema.converter;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
 import sistema.modelos.Equipe;
 
 //@FacesConverter(value = "classeConverter")    
-//@FacesConverter(forClass = Classe.class)
+@FacesConverter(forClass = Equipe.class)
 public class EquipeConverter implements Converter {
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
